9b50e5d6646297a300964ab43bd76164e27b8321
Miss-Zulu <khanyisilezulu224@gmail.com>
2024-06-10 21:26:22 +0200

    Added the search functionality, using the API

--- src/advance.js
+++ src/advance.js
@@ -1,9 +1,25 @@
+function refreshData(response) {
+  let temperatureElement = document.querySelector("#feel");
+  let temperature = response.data.temperature.current;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+
+  let cityElement = document.querySelector("#place");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "9dcefb646a0cd4b5f33ct484b05bcoe0";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshData);
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-button");
   console.log(searchInput.value);
-  let cityElement = document.querySelector("#place");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
